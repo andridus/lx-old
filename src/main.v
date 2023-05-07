99243fb7fa9f4591999992e345811642de141c5c
Helder Sousa <helderhenri@gmail.com>
2023-05-07 07:40:05 -0300

    FEAT: add a REPL

--- src/main.v
+++ src/main.v
@@ -1,17 +1,35 @@
 // import parser
 import os
 import lexer
+import repl
 import time
 fn main() {
 	if os.args.len >= 2 {
 		mut sw := time.new_stopwatch()
-		path := os.args[1]
-		content := os.read_file(path) or { panic("File 'example.ex' not exists on root dir")}
-		elapsed := sw.elapsed().milliseconds()
-		println('\nRead file `$path` at ${elapsed}ms')
-		_ = lexer.new(content)
-		println('Compiled `$path` at ${sw.elapsed().milliseconds()-elapsed}ms')
+		command := os.args[1]
+		match command {
+			"repl" {
+
+				repl.start()
+			}
+			"build" {
+				if os.args.len == 3 {
+					path := os.args[2]
+					content := os.read_file(path) or { panic("File '$path' not exists on root dir")}
+					elapsed := sw.elapsed().milliseconds()
+					println('\nRead file `$path` at ${elapsed}ms')
+					mut le := lexer.new(content)
+					le.generate_tokens()
+					println('Compiled `$path` at ${sw.elapsed().milliseconds()-elapsed}ms')
+				}else{
+					println("File is need to lexer")
+				}
+			}
+			else {
+				println("Basic commands `lx build {file.ex}` or `lx repl`")
+			}
+		}
 	} else {
-		println("File is need to lexer")
+		println("Basic commands `lx build {file.ex}` or `lx repl`")
 	}
 }
