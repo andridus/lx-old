b35a3a57d32422cdf9869e7c778bb3a36c5a9424
Helder Sousa <helderhenri@gmail.com>
2023-07-03 12:57:24 -0300

    TEST: Example with math operations

---
+++ examples/operations/operations.ex
@@ -0,0 +1,25 @@
+defmodule Operations do
+  def main() do
+    sum(1,2)
+    sub(2,1)
+    mul(3,2)
+    div(2,2)
+
+    3 = sum(1,2)
+    1 = sub(2,1)
+    6 = mul(3,2)
+    4 = div(2,2)
+  end
+  def sum(a, b) do
+    a + b
+  end
+  def sub(a, b) do
+    a - b
+  end
+  def mul(a, b) do
+    a * b
+  end
+  def div(a, b) do
+    a / b
+  end
+end
